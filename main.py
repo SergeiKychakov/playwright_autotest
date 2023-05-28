acc348504fad557819492fff20c3d7fdf0359ef2
serg <sergei.kychakov@gmail.ru>
2023-05-28 21:19:57 +0500

    first initial

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+from playwright.sync_api import Playwright, sync_playwright, expect
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://playwright-todomvc.antonzimaiev.repl.co/#/")
+    page.get_by_placeholder("What needs to be done?").click()
+    page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
+    page.get_by_placeholder("What needs to be done?").press("Enter")
+    # ---------------------
+    context.close()
+    browser.close()
+with sync_playwright() as playwright:
+    run(playwright)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#if __name__ == '__main__':
+#    print_hi('PyCharm')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
