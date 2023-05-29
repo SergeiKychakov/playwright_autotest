2440560d3174d92383f87952c1670eac5d7e7ca1
serg <sergei.kychakov@gmail.ru>
2023-05-29 17:55:38 +0500

    add_API_test

--- test_todo.py
+++ test_todo.py
@@ -151,4 +151,30 @@ def test_intercepted(page: Page):
     expect(sidebar.get_by_role('link')).to_contain_text(["open", "solutions"])
 
 
+def test_inventory(page):
+    response = page.request.get('https://petstore.swagger.io/v2/store/inventory')
+    print(response.status)
+    print(response.json())
+
+
+def test_add_user(page):
+    data = [
+              {
+                "id": 9743,
+                "username": "fsd",
+                "firstName": "fff",
+                "lastName": "ggg",
+                "email": "bbb",
+                "password": "tt",
+                "phone": "333",
+                "userStatus": 0
+              }
+            ]
+    header = {
+        'accept': 'application/json',
+        'content-Type': 'application/json'
+    }
+    response = page.request.post('https://petstore.swagger.io/v2/user/createWithArray', data=data, headers=header)
+    print(response.status)
+    print(response.json())
 
